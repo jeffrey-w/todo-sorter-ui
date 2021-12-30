cfb064f140634a9bba322441c900722586612fc0
jeffrey-w <39098167+jeffrey-w@users.noreply.github.com>
2021-12-30 10:57:30 -0500

    create new test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,10 @@
-import { render, screen } from '@testing-library/react';
 import App from './App';
+import { Provider } from 'react-redux';
+import { store } from './store'
+import { render, screen } from '@testing-library/react';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+test('renders app bar title', () => {
+  render(<Provider store={store} ><App /></Provider>);
+  const linkElement = screen.getByText(/TODOsorter/i);
   expect(linkElement).toBeInTheDocument();
 });
