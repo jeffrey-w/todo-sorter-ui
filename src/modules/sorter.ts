437a2927bfaf112ae8557fd98f6e91109bc61af1
jeffrey-w <39098167+jeffrey-w@users.noreply.github.com>
2022-03-11 21:48:47 -0500

    document model classes

--- src/modules/sorter.ts
+++ src/modules/sorter.ts
@@ -1,5 +1,8 @@
 import { Todo, TodoList } from "./todo";
 
+/**
+ * The `Sorter` class provides services for priortizing the `Todo`s in a `TodoList`.
+ */
 export class Sorter {
 
     private _lists: TodoList[];
@@ -7,6 +10,12 @@ export class Sorter {
     private _outer: number;
     private _inner: number;
 
+    /**
+     * Creates a new `Sorter` with the specified `todos`.
+     * 
+     * @param todos A collection of `Todo`s.
+     * @throws An `Error` if the specified collection of `todos` is `null` or contains `null` elements.
+     */
     constructor(todos: Todo[]) {
         this._lists = TodoList.listsFrom(todos);
         this._cursor = 0;
@@ -14,32 +23,64 @@ export class Sorter {
         this._inner = 1;
     }
     
+    /**
+     * Provides the `Todo`s being prioritized by this `Sorter`, partitioned on the rank they exhibit.
+     */
     get lists(): TodoList[] {
         return [...this._lists];
     }
 
+    /**
+     * Provides the `Todo` presently being prioritized by this `Sorter`.
+     * 
+     * @returns {Todo} The `Todo` that is currently being prioritized.
+     */
     getCurrent(): Todo {
         return this._lists[this._cursor].getAt(this._outer);
     }
-
+    
+    /**
+     * Provides the next `Todo` to rank against the one currently being prioritized by this `Sorter`.
+     * 
+     * @returns {Todo} The next `Todo` to rank agains the current one.
+     */
     getNext(): Todo {
         return this._lists[this._cursor].getAt(this._inner);
     }
 
+    /**
+     * Privides the `Todo`s being prioritized by this `Sorter`.
+     * 
+     * @returns {Todo[]} The `Todo`s that this `Sorter` is prioritizing.
+     */
     getTodos(): Todo[] {
         return this._lists.flatMap(list =>
             list.todos).sort((one, two) =>
                 two.priority - one.priority);
     }
 
+    /**
+     * Induces this `Sorter` to increment the rank of the `Todo` currently being prioritized.
+     * 
+     * @throws An `Error` if incrementing the priority of the current `Todo` results in arithmetic overflow.
+     */
     incrementOuter() {
         this._lists = incrementAt(this._lists, this._cursor, this._outer);
     }
 
+    /**
+     * Induces this `Sorter` to increment the rank of the `Todo` that is being ranked against the one currently being
+     * prioritized.
+     * 
+     * @throws An `Error` if incrementing the priority of the next `Todo` results in arithmetic overflow.
+     */
     incrementInner() {
         this._lists = incrementAt(this._lists, this._cursor, this._inner);
     }
 
+    /**
+     * Induces this `Sorter` to increment its position such that the next `Todo` becomes the current one.
+     */
     advance() {
         this._inner++;
         if (this._inner >= this._lists[this._cursor].size()) {
@@ -55,18 +96,38 @@ export class Sorter {
         }
     }
 
+    /**
+     * Determines whether this `Sorter` has another `Todo` to prioritize.
+     * 
+     * @returns {boolean} `true` if this `Sorter` has another `Todo` to prioritize.
+     */
     hasNext(): boolean {
         return this._cursor < this._lists.length;
     }
 
+    /**
+     * Determines whether this `Sorter` has prioritized every `Todo` under consideration.
+     * 
+     * @returns `true` if every `Todo` under consideration has been prioritized.
+     */
     isSorted(): boolean {
         return this._lists.every(list => list.size() === 1);
     }
 
+    /**
+     * Preserves the state of this `Sorter`.
+     * 
+     * @returns {Memento} A new `Memento` capturing the current state of this `Sorter`.
+     */
     save(): Memento {
         return new Memento(this._lists, this._cursor, this._outer, this._inner);
     }
 
+    /**
+     * Returns the state of this `Sorter` to that captured by the specified `memento`.
+     * 
+     * @param {Memento} memento A `Memento` that holds some previous state of this `Sorter`.
+     */
     restore(memento: Memento) {
         this._lists = memento.lists;
         this._cursor = memento.cursor;
@@ -82,6 +143,9 @@ function incrementAt(lists: TodoList[], cursor: number, index: number) {
     return clone;
 }
 
+/**
+ * The `Memento` class represents a snapshot of the state of a `Sorter`.
+ */
 export class Memento {
 
     private _lists: TodoList[];
@@ -89,6 +153,17 @@ export class Memento {
     private _outer: number;
     private _inner: number;
 
+    /**
+     * Creates a new `Memento` with the specified `lists`, `cursor`, and `outer` and `inner` pointers.
+     * 
+     * @param {TodoList[]} lists A collection of `TodoList`s being prioritized by the `Sorter` creating this `Memento`.
+     * @param {number} cursor The index of the `TodoList` currently being prioritized by the `Sorter` creating this
+     * `Memento`.
+     * @param {number} outer The index of the `Todo` presently being prioritized by the `Sorter` creating this
+     * `Memento`.
+     * @param {number} inner The index of the `Todo` being ranked against the one currently being prioritized by the
+     * `Sorter` creating this `Memento`.
+     */
     constructor(lists: TodoList[], cursor: number, outer: number, inner: number) {
         this._lists = lists;
         this._cursor = cursor;
@@ -96,18 +171,31 @@ export class Memento {
         this._inner = inner;
     }
 
+    /**
+     * Provides the collection of `TodoList`s being prioritized by the `Sorter` that created this `Memento`.
+     */
     get lists(): TodoList[] {
         return this._lists;
     }
 
+    /**
+     * Provides the index of the `TodoList` currently being prioritized by the `Sorter` that created this `Memento`.
+     */
     get cursor(): number {
         return this._cursor;
     }
 
+    /**
+     * Provides the index of the `Todo` presently being prioritized by the `Sorter` that created this `Memento`.
+     */
     get outer(): number {
         return this._outer;
     }
 
+    /**
+     * Provides the index of the `Todo` being ranked against the one currently being prioritized by the `Sorter` that
+     * created this `Memento`.
+     */
     get inner(): number {
         return this._inner;
     }
--- src/modules/todo.ts
+++ src/modules/todo.ts
@@ -1,76 +1,199 @@
+/**
+ * The `Todo` class represents a task that needs to be completed. It is characterized by a descriptive name and a
+ * priority that indicates its relative importance.
+ */
 export class Todo {
+
+    private static whitespacePattern: RegExp = /^\s*$/;
+
+    /**
+     * Creates a new `Todo` with the specified `name`.
+     * 
+     * @param {string} name A natural language characterization of the new `Todo`.
+     * @returns {Todo} A new `Todo`.
+     * @throws An `Error` if the specified `name` is `null` or empty.
+     */
+    public static create(name: string): Todo {
+        return new Todo(0, name);
+    }
+    
+    private static guardAgainstNegative(priority: number): number {
+        if (priority < 0) {
+            throw Error("priority cannot be less than 0.");
+        }
+        return priority;
+    }
+
+    private static guardAgainstNullOrEmpty(name: string): string {
+        if (this.whitespacePattern.test(name)){
+            throw new Error("name cannot be null or empty.");
+        }
+        return name;
+    }
 
     private _priority: number;
     private _name: string;
 
-    constructor(priority: number, name: string) {
-        this._priority = priority;
-        this._name = name;
+    private constructor(priority: number, name: string) {
+        this._priority = Todo.guardAgainstNegative(priority);
+        this._name = Todo.guardAgainstNullOrEmpty(name);
     }
 
+    /**
+     * Provides the relative importance of this `Todo`.
+     */
     get priority(): number {
         return this._priority;
     }
 
+    /**
+     * Provides a natural language characterization of this `Todo`.
+     */
     get name(): string {
         return this._name;
     }
 
+    /**
+     * Provides a new `Todo` with the same name and a priority that is one greater than this one.
+     * 
+     * @returns {Todo} A new `Todo` with a higher priority than this one.
+     * @throws An `Error` if incrementing the priority of this `Todo` results in arithmetic overflow.
+     */
     increment(): Todo {
         return new Todo(this.priority + 1, this.name);
     }
 
 }
 
+/**
+ * The `TodoList` class represents a collection of `Todo`s to be prioritized.
+ */
 export class TodoList {
 
+    /**
+     * Creats a new collection of `TodoList`s each containing the subset of the specified `todos` that have the same
+     * priority as each other.
+     * 
+     * @param {Todo[]} todos A collection of `Todo`s.
+     * @returns {TodoList[]} A collection of `TodoList`s each containing `Todo`s with equal priority.
+     * @throws An `Error` if the specified collection of `todos` is `null` or contains `null` elements.
+     */
     static listsFrom(todos: Todo[]): TodoList[] {
         const list = new TodoList(todos);
         return list.sort();
     }
 
+    /**
+     * Creats a new collection of `Todo`s containing every element from the specified `lists`.
+     * 
+     * @param {TodoList[]} lists A collection of `TodoList`s.
+     * @returns {Todo[]} A collection of all of the `Todo`s in each of the specified `lists`.
+     * @throws An `Error` if the specified `lists` is `null` or contains `null` elements.
+     */
     static merge(lists: TodoList[]): Todo[] {
         return lists.flatMap(list => list.todos);
     }
 
-    private _todos: Todo[]
-
-    constructor(todos: Todo[]) {
-        this._todos = [...todos];
+    private static guardAgainstNullTodos(todos: Todo[]): Todo[] {
+        todos.forEach(todo => {
+            if (todo == null) {
+                throw new Error("todos cannot contain null elements.");
+            }
+        });
+        return todos;
     }
 
+    private _todos: Todo[]
+
+    private constructor(todos: Todo[], copy: boolean = true) {
+        if (copy) {
+            this._todos = [...TodoList.guardAgainstNullTodos(todos)];
+        }
+        this._todos = TodoList.guardAgainstNullTodos(todos);
+    }
+
+
+    /**
+     * Provides the `Todo`s in this `TodoList`.
+     */
     get todos(): Todo[] {
         return this._todos;
     }
 
+    /**
+     * Provides a partitioning of this `TodoList` on the priority of the `Todo`s it contains.
+     * 
+     * @returns {TodoList[]} A collection of `TodoList`s each containing the subset of the `Todo`s in this `TodoList`
+     * that have the same priority as each other.
+     */
     sort(): TodoList[] {
-        return Object.values(groupBy(this.todos, todo => todo.priority))
+        return Object.values(this.groupBy(this.todos, todo => todo.priority))
             .map(todos => new TodoList(todos));
     }
 
-    incrementAt(index: number) {
-        const clone = [...this.todos];
-        clone[index] = clone[index].increment();
-        return new TodoList(clone);
+    private groupBy<K extends keyof any, V>(array: V[], keySelector: (v: V) => K): Record<K, V[]> {
+        return array.reduce((groups, element) => {
+            const key = keySelector(element);
+            groups[key] = groups[key] ?? [];
+            groups[key].push(element);
+            return groups;
+        }, {} as Record<K, V[]>);
     }
 
+    /**
+     * Provides a new `Todo` with the same name and a priority that is one greater than this one.
+     * 
+     * @returns {Todo} A new `Todo` with a higher priority than this one.
+     * @throws An `Error` if incrementing the priority of this `Todo` results in arithmetic overflow.
+     */
+
+    /**
+     * Provides a new `TodoList` with a `Todo` at the specified `index` that is one greater than the `Todo` at that
+     * position in this `TodoList`.
+     * 
+     * @param {number} index The position of the `Todo` to increment.
+     * @returns {TodoList} A new `TodoList`.
+     * @throws An `Error` if the specified `index` is less than zero or greater than or equal to the size of this
+     * `TodoList`.
+     * @throws An `Error` if incrementing the priority of the `Todo` at the specified `index` results in arithmetic
+     * overflow.
+     */
+
+    /**
+     * Provides a new `TodoList` with the same elements as this one except for the `Todo` at the specified `index`,
+     * which is replaced with one with the same name and a priority that is one greater.
+     * 
+     * @param {number} index 
+     * @returns {TodoList} A new `TodoList`.
+     * @throws An `Error` if the specified `index` is less than zero or greater than or equal to the size of this
+     * `TodoList`, or if incrementing the priority of the `Todo` at the specified `index` results in arithmetic
+     * overflow.
+     */
+    incrementAt(index: number): TodoList {
+        const clone = [...this.todos];
+        clone[index] = clone[index].increment();
+        return new TodoList(clone, false);
+    }
+
+    /**
+     * Provides the `Todo` at the specified `index` of this `TodoList`.
+     * 
+     * @param {number} index The position of the `Todo` to retreive.
+     * @returns {Todo} The `Todo` at the specified `index`.
+     * @throws An `Error` if the specified `index` is less than zero or greater than or equal to the size of this
+     * `TodoList`.
+     */
     getAt(index: number): Todo {
         return this.todos[index];
     }
 
+    /**
+     * Provides the number of `Todo`s in this `TodoList`.
+     * 
+     * @returns {number} The size of this `TodoList`.
+     */
     size(): number {
-        return this._todos.length;
+        return this.todos.length;
     }
 
-}
-
-function groupBy<K extends keyof any, V>(array: V[], keySelector: (v: V) => K) {
-    return array.reduce((groups, element) => {
-        const key = keySelector(element);
-        if (!groups[key]) {
-            groups[key] = [];
-        }
-        groups[key].push(element);
-        return groups;
-    }, {} as Record<K, V[]>);
 }
